8be0c8c58ad6779b88c39a7ca0e29f38d514414e
Putrabot <77587385+putraelf@users.noreply.github.com>
2021-03-05 12:53:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Welcome In\nputra Botenzet', {
+CFonts.say('Welcome In\n Botenzet', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
